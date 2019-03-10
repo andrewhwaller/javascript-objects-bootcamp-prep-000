0667cf911b7c6b06fe7ade34553a40c89218a234
Andrew Waller <andrewhwaller@gmail.com>
2019-03-10 01:29:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = Object.assign({}, { artist: 'song' })
+var artist = 'Phil Ochs'
 
-function updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi") {
-  return Object.assign({}, playlist, { ["Phil Ochs"]: "Here's to the State of Mississippi" })
-  return playlist
+function updatePlaylist(playlist,artist, songtitle) {
+  playlist.artist = 'My Bloody Valentine'
 }
